0cef2f375a8dd12a1984f1027653ebe0361cf90a
Mandr1k <149248715+Mandr1k@users.noreply.github.com>
2023-11-06 17:51:31 +0200

    First commit

---
+++ оп лр4.cpp
@@ -0,0 +1,30 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <math.h>
+#define _USE_MATH_DEFINES
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double amount, size;
+	cout << "Введіть кількість учнів =";
+	cin >> amount;
+	cout << "Введіть розмір шернги =";
+	cin >> size;
+	int full = amount / size;
+	int rest = (int)amount % (int)size;
+	cout << full << endl;
+	cout << rest;
+
+	double x, y, z;
+	cin >> x;
+	cin >> y;
+	cin >> z;
+	double a = (3 * abs(y) - sqrt(pow(x, 3) + 4 * y)) / (10 * x - (y / (pow(z, 3) + (pow(x, 4) / 4))));
+	printf("Результат виразу = %lf", a);
+
+	double b = (exp(2) + pow(y, -2) * cos(45) * x) / (5 * x + 1) + sin(pow(x, 2) + y);
+	printf("\nРезультат виразу = %lf", b);
+}
